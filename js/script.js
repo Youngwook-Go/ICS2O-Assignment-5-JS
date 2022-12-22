194bb286424e92a14c761b6b0070cdd2c2e4b3d1
Youngwook Go <113055584+Youngwook-Go@users.noreply.github.com>
2022-12-22 02:36:36 +0000

    111

--- js/script.js
+++ js/script.js
@@ -18,4 +18,25 @@ if (navigator.serviceWorker) {
 
 // function description
 
-function submit() {}
+function update() {
+  console.log("submit")
+  const number = parseInt(document.getElementById("user-input").value)
+  console.log("input is : " + number)
+  var count = 0
+  var answer = 0
+  var denominator = 1
+  var sign = 1
+  while (number >= count)
+  {
+    answer = answer + (4 / denominator)
+    sign = (Math.abs(denominator) / denominator) * -1
+    denominator = (Math.abs(denominator) + 2) * sign
+
+    count = count + 1
+    console.log("answer is : " + answer.toFixed(2))
+    console.log("denominator is : " + denominator)
+    console.log("count is : " + count)
+  }
+  document.getElementById("answer").innerHTML = "The answer is : " + answer.toFixed(2)
+  console.log("end")
+}
